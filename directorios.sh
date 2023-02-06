382697371dc05d3514238ba9f5d368b0967c718b
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2023-02-06 12:07:40 +0100

    Update and rename directirios.sh to directorios.sh

--- directorios.sh
+++ directorios.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 mkdir -p /media/alurod1862/Publico/{1ESO,2ESO,3ESO,4ESO,1BATCH,2BATCH,1DAM,2DAM}
+mkdir -p /media/alurod1862/Publico/Politecnic/{1ESO,2ESO,3ESO,4ESO,1BATCH,2BATCH,1DAM,2DAM}
+
+
