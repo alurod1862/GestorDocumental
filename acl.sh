5fc165b0f70d8f1749b3286c54b5f36250ade25b
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2023-02-06 11:44:37 +0100

    Create acl.sh

---
+++ acl.sh
@@ -0,0 +1,155 @@
+#/bin/bash
+
+#1ESO
+
+setfacl -m d:g:LecturaEjecucion_1ESO:rx /media/alurod1862/Publico/1ESO
+setfacl -m g:LecturaEjecucion_1ESO:rx /media/alurod1862/Publico/1ESO
+setfacl -m d:g:AcessoTotal_1ESO:rwx /media/alurod1862/Publico/1ESO
+setfacl -m g:AcessoTotal_1ESO:rwx /media/alurod1862/Publico/1ESO
+setfacl -m d:g:LecturaEjecucion_1ESO:x /media/alurod1862/Publico/1ESO
+setfacl -m g:LecturaEjecucion_1ESO:x /media/alurod1862/Publico/1ESO
+setfacl -m d:other:0 /media/alurod1862/Publico/1ESO
+setfacl -m other:0 /media/alurod1862/Publico/1ESO
+setfacl -m d:mask:rwx /media/alurod1862/Publico/1ESO
+setfacl -m mask:rwx /media/alurod1862/Publico/1ESO
+
+
+#2ESO
+
+setfacl -m d:g:LecturaEjecucion_2ESO:rx /media/alurod1862/Publico/2ESO
+setfacl -m g:LecturaEjecucion_2ESO:rx /media/alurod1862/Publico/2ESO
+setfacl -m d:g:AcessoTotal_2ESO:rwx /media/alurod1862/Publico/2ESO
+setfacl -m g:AcessoTotal_2ESO:rwx /media/alurod1862/Publico/2ESO
+setfacl -m d:g:LecturaEjecucion_2ESO:x /media/alurod1862/Publico/2ESO
+setfacl -m g:LecturaEjecucion_2ESO:x /media/alurod1862/Publico/2ESO
+setfacl -m d:other:0 /media/alurod1862/Publico/2ESO
+setfacl -m other:0 /media/alurod1862/Publico/2ESO
+setfacl -m d:mask:rwx /media/alurod1862/Publico/2ESO
+setfacl -m mask:rwx /media/alurod1862/Publico/2ESO
+
+
+#3ESO
+
+setfacl -m d:g:LecturaEjecucion_3ESO:rx /media/alurod1862/Publico/3ESO
+setfacl -m g:LecturaEjecucion_3ESO:rx /media/alurod1862/Publico/3ESO
+setfacl -m d:g:AcessoTotal_3ESO:rwx /media/alurod1862/Publico/3ESO
+setfacl -m g:AcessoTotal_3ESO:rwx /media/alurod1862/Publico/3ESO
+setfacl -m d:g:LecturaEjecucion_3ESO:x /media/alurod1862/Publico/3ESO
+setfacl -m g:LecturaEjecucion_3ESO:x /media/alurod1862/Publico/3ESO
+setfacl -m d:other:0 /media/alurod1862/Publico/3ESO
+setfacl -m other:0 /media/alurod1862/Publico/3ESO
+setfacl -m d:mask:rwx /media/alurod1862/Publico/3ESO
+setfacl -m mask:rwx /media/alurod1862/Publico/3ESO
+
+
+#4ESO
+
+
+setfacl -m d:g:LecturaEjecucion_4ESO:rx /media/alurod1862/Publico/4ESO
+setfacl -m g:LecturaEjecucion_4ESO:rx /media/alurod1862/Publico/4ESO
+setfacl -m d:g:AcessoTotal_4ESO:rwx /media/alurod1862/Publico/4ESO
+setfacl -m g:AcessoTotal_4ESO:rwx /media/alurod1862/Publico/4ESO
+setfacl -m d:g:LecturaEjecucion_4ESO:x /media/alurod1862/Publico/4ESO
+setfacl -m g:LecturaEjecucion_4ESO:x /media/alurod1862/Publico/4ESO
+setfacl -m d:other:0 /media/alurod1862/Publico/4ESO
+setfacl -m other:0 /media/alurod1862/Publico/4ESO
+setfacl -m d:mask:rwx /media/alurod1862/Publico/4ESO
+setfacl -m mask:rwx /media/alurod1862/Publico/4ESO
+
+
+
+#1BATCH
+
+setfacl -m d:g:LecturaEjecucion_1BATCH:rx /media/alurod1862/Publico/1BATCH
+setfacl -m g:LecturaEjecucion_1BATCH:rx /media/alurod1862/Publico/1BATCH
+setfacl -m d:g:AcessoTotal_1BATCH:rwx /media/alurod1862/Publico/1BATCH
+setfacl -m g:AcessoTotal_1BATCH:rwx /media/alurod1862/Publico/1BATCH
+setfacl -m d:g:LecturaEjecucion_1BATCH:x /media/alurod1862/Publico/1BATCH
+setfacl -m g:LecturaEjecucion_1BATCH:x /media/alurod1862/Publico/1BATCH
+setfacl -m d:other:0 /media/alurod1862/Publico/1BATCH
+setfacl -m other:0 /media/alurod1862/Publico/1BATCH
+setfacl -m d:mask:rwx /media/alurod1862/Publico/1BATCH
+setfacl -m mask:rwx /media/alurod1862/Publico/1BATCH
+
+
+
+#2BATCH
+setfacl -m d:g:LecturaEjecucion_2BATCH:rx /media/alurod1862/Publico/2BATCH
+setfacl -m g:LecturaEjecucion_2BATCH:rx /media/alurod1862/Publico/2BATCH
+setfacl -m d:g:AcessoTotal_2BATCH:rwx /media/alurod1862/Publico/2BATCH
+setfacl -m g:AcessoTotal_2BATCH:rwx /media/alurod1862/Publico/2BATCH
+setfacl -m d:g:LecturaEjecucion_2BATCH:x /media/alurod1862/Publico/2BATCH
+setfacl -m g:LecturaEjecucion_2BATCH:x /media/alurod1862/Publico/2BATCH
+setfacl -m d:other:0 /media/alurod1862/Publico/2BATCH
+setfacl -m other:0 /media/alurod1862/Publico/2BATCH
+setfacl -m d:mask:rwx /media/alurod1862/Publico/2BATCH
+setfacl -m mask:rwx /media/alurod1862/Publico/2BATCH
+
+
+
+#1DAM
+
+setfacl -m d:g:LecturaEjecucion_1DAM:rx /media/alurod1862/Publico/1DAM
+setfacl -m g:LecturaEjecucion_1DAM:rx /media/alurod1862/Publico/1DAM
+setfacl -m d:g:AcessoTotal_1DAM:rwx /media/alurod1862/Publico/1DAM
+setfacl -m g:AcessoTotal_1DAM:rwx /media/alurod1862/Publico/1DAM
+setfacl -m d:g:LecturaEjecucion_1DAM:x /media/alurod1862/Publico/1DAM
+setfacl -m g:LecturaEjecucion_1DAM:x /media/alurod1862/Publico/1DAM
+setfacl -m d:other:0 /media/alurod1862/Publico/1DAM
+setfacl -m other:0 /media/alurod1862/Publico/1DAM
+setfacl -m d:mask:rwx /media/alurod1862/Publico/1DAM
+setfacl -m mask:rwx /media/alurod1862/Publico/1DAM
+
+
+
+#2DAM
+
+setfacl -m d:g:LecturaEjecucion_2DAM:rx /media/alurod1862/Publico/2DAM
+setfacl -m g:LecturaEjecucion_2DAM:rx /media/alurod1862/Publico/2DAM
+setfacl -m d:g:AcessoTotal_2DAM:rwx /media/alurod1862/Publico/2DAM
+setfacl -m g:AcessoTotal_2DAM:rwx /media/alurod1862/Publico/2DAM
+setfacl -m d:g:LecturaEjecucion_2DAM:x /media/alurod1862/Publico/2DAM
+setfacl -m g:LecturaEjecucion_2DAM:x /media/alurod1862/Publico/2DAM
+setfacl -m d:other:0 /media/alurod1862/Publico/2DAM
+setfacl -m other:0 /media/alurod1862/Publico/2DAM
+setfacl -m d:mask:rwx /media/alurod1862/Publico/2DAM
+setfacl -m mask:rwx /media/alurod1862/Publico/2DAM
+
+
+#ALUMANADO
+
+setfacl -m d:g:Alumnado:r /media/alurod1862/Publico
+setfacl -m g:Alumnado:r /media/alurod1862/Publico
+setfacl -m d:other:0 /media/alurod1862/Publico
+setfacl -m other:0 /media/alurod1862/Publico
+setfacl -m d:mask:rwx /media/alurod1862/Publico
+setfacl -m mask:rwx /media/alurod1862/Publico
+
+
+
+#PROFESORADO
+
+setfacl -m d:g:Profesorado:r /media/alurod1862/Publico
+setfacl -m g:Profesorado:r /media/alurod1862/Publico
+setfacl -m d:other:0 /media/alurod1862/Publico
+setfacl -m other:0 /media/alurod1862/Publico
+setfacl -m d:mask:rwx /media/alurod1862/Publico
+setfacl -m mask:rwx /media/alurod1862/Publico
+
+
+
+#ADINISTRADOR
+
+
+setfacl -m d:g:Administrador:rwx /media/alurod1862/Publico
+setfacl -m g:Administrador:rwx /media/alurod1862/Publico
+setfacl -m d:other:0 /media/alurod1862/Publico
+setfacl -m other:0 /media/alurod1862/Publico
+setfacl -m d:mask:rwx /media/alurod1862/Publico
+setfacl -m mask:rwx /media/alurod1862/Publico
+
+
+
+
+
+
