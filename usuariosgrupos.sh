cad17dc90600f0e7ca1d384bd221ed0b511721cc
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2023-02-07 13:15:52 +0100

    Update usuariosgrupos.sh

--- usuariosgrupos.sh
+++ usuariosgrupos.sh
@@ -1,38 +1,45 @@
 #!/bin/bash
 
+
 groupadd 1ESO
 groupadd LecturaEjecucion_1ESO
-groupadd AccesoTotal_1ESO
+groupadd AcessoTotal_1ESO
 groupadd SinPermiso_1ESO
+
 
 groupadd 2ESO
 groupadd LecturaEjecucion_2ESO
-groupadd AccesoTotal_2ESO
+groupadd AcessoTotal_2ESO
 groupadd SinPermiso_2ESO
+
 
 groupadd 3ESO
 groupadd LecturaEjecucion_3ESO
-groupadd AccesoTotal_3ESO
+groupadd AcessoTotal_3ESO
 groupadd SinPermiso_3ESO
+
 
 groupadd 4ESO
 groupadd LecturaEjecucion_4ESO
-groupadd AccesoTotal_4ESO
+groupadd AcessoTotal_4ESO
 groupadd SinPermiso_4ESO
+
 
 groupadd 1BATCH
 groupadd LecturaEjecucion_1BATCH
-groupadd AccesoTotal_1BATCH
+groupadd AcessoTotal_1BATCH
 groupadd SinPermiso_1BATCH
+
 
 groupadd 2BATCH
 groupadd LecturaEjecucion_2BATCH
-groupadd AccesoTotal_2BATCH
+groupadd AcessoTotal_2BATCH
 groupadd SinPermiso_2BATCH
+
 
 groupadd 1DAM
 groupadd LecturaEjecucion_1DAM
-groupadd AccesoTotal_1DAM
+groupadd AcessoTotal_1DAM
 groupadd SinPermiso_1DAM
 
 
@@ -49,9 +56,9 @@ useradd -m -s /bin/bash -G 4ESO USU1_4ESO
 useradd -m -s /bin/bash -G 1BATCH USU1_1BATCH
 useradd -m -s /bin/bash -G 2BATCH USU1_2BATCH
 useradd -m -s /bin/bash -G 1DAM USU1_1DAM
-useradd -m -s /bin/bash -G Alumnado Alumnado
-useradd -m -s /bin/bash -G Profesorado Profesorado
-useradd -m -s /bin/bash -G Administrador Administrador
+useradd -m -s /bin/bash -G Alumnado alumnado
+useradd -m -s /bin/bash -G Profesorado profesorado
+useradd -m -s /bin/bash -G Administrador administrador
 
 
 
@@ -69,9 +76,8 @@ usermod -a -G SinPermiso_1ESO,SinPermiso_2ESO,SinPermiso_3ESO,SinPermiso_4ESO,Le
 
 usermod -a -G AcessoTotal_1ESO,AcessoTotal_2ESO,AcessoTotal_3ESO,AcessoTotal_4ESO,AcessoTotal_1BATCH,,AcessoTotal_2BATCH,,AcessoTotal_1DAM,Alumnado,Profesorado,Administrador USU1_1DAM
 
-usermod -a -G AcessoTotal_1ESO,AcessoTotal_2ESO,AcessoTotal_3ESO,AcessoTotal_4ESO,AcessoTotal_1BATCH,,AcessoTotal_2BATCH,,AcessoTotal_1DAM,Alumnado,Profesorado Administrador
+usermod -a -G AcessoTotal_1ESO,AcessoTotal_2ESO,AcessoTotal_3ESO,AcessoTotal_4ESO,AcessoTotal_1BATCH,,AcessoTotal_2BATCH,,AcessoTotal_1DAM,Alumnado,Profesorado administrador
 
-usermod -a -G SinPermiso_1ESO,SinPermiso_2ESO,SinPermiso_3ESO,SinPermiso_4ESO,SinPermiso_1BATCH,SinPermiso_2BATCH,SinPermiso_1DAM,Alumnado Alumnado
+usermod -a -G SinPermiso_1ESO,SinPermiso_2ESO,SinPermiso_3ESO,SinPermiso_4ESO,SinPermiso_1BATCH,SinPermiso_2BATCH,SinPermiso_1DAM,Alumnado alumnado
 
-usermod -a -G SinPermiso_1ESO,SinPermiso_2ESO,SinPermiso_3ESO,SinPermiso_4ESO,SinPermiso_1BATCH,SinPermiso_2BATCH,SinPermiso_1DAM,Profesorado Profesorado
-
+usermod -a -G SinPermiso_1ESO,SinPermiso_2ESO,SinPermiso_3ESO,SinPermiso_4ESO,SinPermiso_1BATCH,SinPermiso_2BATCH,SinPermiso_1DAM,Profesorado profesorado
