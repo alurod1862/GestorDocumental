3d908e740d74ce683c6ed37ccb7cec1da2c4719f
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2023-02-07 10:47:20 +0100

    Create borrarusuariosgrupos.sh

---
+++ borrarusuariosgrupos.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+userdel USU1_1ESO
+userdel USU1_2ESO
+userdel USU1_3ESO
+userdel USU1_4ESO
+userdel USU1_1BATCH
+userdel USU1_2BATCH
+userdel USU1_1DAM
+userdel USU1_2DAM
+userdel Alumando
+userdel Profesorado
+userdel Administrador
+
+groupdel 1ESO
+groupdel LecturaEjecucion_1ESO
+groupdel AccesoTotal_1ESO
+groupdel SinPermiso_1ESO
+
+groupdel 2ESO
+groupdel LecturaEjecucion_2ESO
+groupdel AccesoTotal_2ESO
+groupdel SinPermiso_2ESO
+
+groupdel 3ESO
+groupdel LecturaEjecucion_3ESO
+groupdel AccesoTotal_3ESO
+groupdel SinPermiso_3ESO
+
+groupdel 4ESO
+groupdel LecturaEjecucion_4ESO
+groupdel AccesoTotal_4ESO
+groupdel SinPermiso_4ESO
+
+groupdel 1BATCH
+groupdel LecturaEjecucion_1BATCH
+groupdel AccesoTotal_1BATCH
+groupdel SinPermiso_1BATCH
+
+groupdel 2BATCH
+groupdel LecturaEjecucion_2BATCH
+groupdel AccesoTotal_2BATCH
+groupdel SinPermiso_2BATCH
+
+groupdel 1DAM
+groupdel LecturaEjecucion_1DAM
+groupdel AccesoTotal_1DAM
+groupdel SinPermiso_1DAM
+
+groupdel 2DAM
+groupdel LecturaEjecucion_2DAM
+groupdel AccesoTotal_2DAM
+groupdel SinPermiso_2DAM
+
+
+groupdel Alumnado
+groupdel Profesorado
+groupdel Administrador
